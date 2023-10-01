8f38a0a6d380e335ca58602a55ed730f024cead1
Inder Singh <i.sgh99@gmail.com>
2023-10-02 03:22:14 +0530

    added prime flex and prime vue

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  css: [
+    "primevue/resources/themes/lara-light-blue/theme.css",
+    "/node_modules/primeflex/primeflex.css",
+  ],
+
+  build: {
+    transpile: ["primevue"],
+  },
+});
